5af4a138a89bd1333a86783b7cfc80db7912a72c
gurpreet.singh <singhgurpreet81205@gmail.com>
2026-05-03 23:53:55 -0400

    add config files

---
+++ taskify-api/src/config/db.js
@@ -0,0 +1,14 @@
+import { MONGO_URL } from "./env.js";
+import mongoose from "mongoose";
+
+export const connectDB = async() => {
+    try{
+        await mongoose.connect(MONGO_URL);
+        console.log("Succesfully conneced to DB");
+    } catch(err){
+        console.log(`MongoDB connection failed : ${err}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ taskify-api/src/config/env.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+if (!process.env.MONGO_URL) {
+  throw new Error("MONGO_URL is not defined in .env");
+}
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in .env");
+}
+
+if (!process.env.JWT_REFRESH_SECRET) {
+  throw new Error("JWT_REFRESH_SECRET is not defined in .env");
+}
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const HTTP_PORT = process.env.HTTP_PORT || 8080;
+export const MONGO_URL = process.env.MONGO_URL;
+export const NODE_ENV = process.env.NODE_ENV || "development";
