39a689e1b4ce3996e6b69090fc8698b9f1402083
gurpreet.singh <singhgurpreet81205@gmail.com>
2026-05-04 20:27:03 -0400

    add zod validation schemas

---
+++ taskify-api/src/validators/authValidator.js
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+export const registerSchema = z.object({
+    name: z.string().min(2, "name must be of atleast 2 characters").max(50, "name should not be more than 50 charactrs"),
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters").regex(/[A-Z]/, "Password must contain at least one uppercase letter").regex(/[0-9]/, "Password must contain at least one number").regex(/[!@#$%^&*]/, "Password must contain at least one special character"),
+    confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {message : "Both the passwords dont match", path: ["confirmPassword"]})
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+})
+
